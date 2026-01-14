6dc5115880645db4ee4bc86fffb1d072b1a46ac9
S Sathvik <154729057+SathvikSimha@users.noreply.github.com>
2026-01-14 22:30:50 +0530

    Updated the game

--- main.py
+++ main.py
@@ -90,11 +90,12 @@ def main():
 
                 # Check survive condition
                 if not game_over and turns >= SURVIVE_TURNS:
-                    game_over = True
                     if HUMAN_IS_PREY:
+                        game_over = True
                         result_text = "You survived! Prey (You) win."
                     else:
-                        result_text = "Time Up! Prey survived. AI wins."
+                        game_over = True
+                        result_text = "Prey survived. AI wins."
 
         # Draw grid + agents
         grid.draw(window)
